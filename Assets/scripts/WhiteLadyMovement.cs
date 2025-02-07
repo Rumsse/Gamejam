00b471b0aed0833764c81c446fa05c769655022f
Rumsse <majafrania@spoko.pl>
2025-02-07 23:46:40 +0100

    tak o

---
+++ Assets/scripts/WhiteLadyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteLadyMovement : MonoBehaviour
+{
+    public Transform player; 
+    public float speed = 3f;
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+
+            transform.position += direction * speed * Time.deltaTime;
+
+            // Opcjonalnie: Obróć przeciwnika w kierunku gracza
+            transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+        }
+    }
+}
